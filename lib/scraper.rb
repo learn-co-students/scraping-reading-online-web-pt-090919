7d95142d6e36aadd75a4c94833e218e2b8132799
Naya  Willis <greedybrain18@gmail.com>
2019-10-17 00:00:54 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,8 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+courses = doc.css('#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .title-oE5vT4').each{|heading| puts heading.text.strip}
+# binding.pry
